fb270863b070c7d9fbbf0f8786d89fb368d261da
Mikael Mykha <info.mickel@gmail.com>
2024-10-05 19:37:55 +0200

    added models and DbContext

---
+++ FrirendZoneHub.Server/Data/ChatAppContext.cs
@@ -0,0 +1,19 @@
+﻿using FriendZoneHub.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FriendZoneHub.Server.Data
+{
+    public class ChatAppContext : DbContext
+    {
+        public ChatAppContext(DbContextOptions<ChatAppContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<ChatRoom> ChatRooms { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ FrirendZoneHub.Server/Models/Message.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FriendZoneHub.Server.Models
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        // Foreign keys
+        public int ChatRoomId { get; set; }
+        public ChatRoom ChatRoom { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
