ee160ae6b35aef36cd8b4e800630b9258b13abaf
Mikael Mykha <info.mickel@gmail.com>
2024-10-08 12:08:07 +0200

    emoji added

--- frirendzonehub.client/src/components/MessageInput.jsx
+++ frirendzonehub.client/src/components/MessageInput.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-import { Box, TextField, Button } from "@mui/material";
+import { Box, TextField, Button, IconButton, Popover } from "@mui/material";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import EmojiPicker from "emoji-picker-react"; // Updated import
 
 const MessageInput = ({ onSend }) => {
   const [newMessage, setNewMessage] = useState("");
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const handleSend = () => {
     if (newMessage.trim()) {
@@ -11,8 +14,26 @@ const MessageInput = ({ onSend }) => {
     }
   };
 
+  const handleEmojiClick = (emojiObject) => {
+    setNewMessage((prev) => prev + emojiObject.emoji);
+  };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
   return (
     <Box display="flex" p={2} bgcolor="#1e1e1e">
+      <IconButton onClick={handleClick}>
+        <EmojiEmotionsIcon style={{ color: "#ffffff" }} />
+      </IconButton>
       <TextField
         fullWidth
         variant="outlined"
@@ -37,6 +58,24 @@ const MessageInput = ({ onSend }) => {
       >
         Send
       </Button>
+
+      {/* Emoji Picker Popover */}
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <EmojiPicker onEmojiClick={handleEmojiClick} />
+      </Popover>
     </Box>
   );
 };
