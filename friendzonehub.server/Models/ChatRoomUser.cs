e68decb2c02f078a658a0773f8bc55b802005ec8
Mikael Mykha <info.mickel@gmail.com>
2024-10-11 16:02:10 +0200

    updated Unitest

---
+++ friendzonehub.server/Models/ChatRoomUser.cs
@@ -0,0 +1,11 @@
+﻿namespace friendzonehub.server.Models
+{
+    public class ChatRoomUser
+    {
+        public int ChatRoomId { get; set; }
+        public ChatRoom ChatRoom { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
